c16ca604fbddf7b601de3f1b9740d13387d314cd
thenaijadev <thenaijadev@gmailcom>
2024-01-20 14:07:38 +0100

    Done

--- lib/features/home/presentation/widgets/my_drawer.dart
+++ lib/features/home/presentation/widgets/my_drawer.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:minimalist_social_app/core/widgets/text_widget.dart';
 
 class MyDrawer extends StatelessWidget {
   const MyDrawer({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Drawer();
+    return Drawer(
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            const DrawerHeader(child: CircleAvatar()),
+            ListTile(
+              leading: Icon(
+                Icons.home,
+                color: Theme.of(context).colorScheme.inversePrimary,
+              ),
+              title: TextWidget(
+                text: "HOME",
+                color: Theme.of(context).colorScheme.secondary,
+                fontWeight: FontWeight.bold,
+              ),
+              onTap: () => Navigator.pop(context),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
